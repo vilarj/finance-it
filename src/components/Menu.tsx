bfab38cf7ed444a457bcdcb782ad4f1e3302dfd2
vilarj <lamaravillasan@outlook.com>
2024-03-12 12:25:57 -0400

    minor update

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Menu() {
-  return <div>Menu</div>;
+  return (
+    <div>
+      <Link href="" target="_blank">
+        Calculadora de Prestamo
+      </Link>
+    </div>
+  );
 }
