fdbd77451a0f31ad9526b9c9caf4b051462c7654
vilarj <lamaravillasan@outlook.com>
2024-04-16 09:32:17 -0400

    Adding new sections in the main page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,17 @@
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <div>
+        <h3>Manten todos tus contactos en un solo lugar</h3>
+      </div>
+
+      <div>
+        <h3>Recordatorio de pagos personalizados</h3>
+      </div>
+
+      <div>
+        <h3>Todo en un solo lugar</h3>
+      </div>
+    </div>
+  );
 }
